ec4bc080f144dbd7bc8cb5b2ba922a0616ddba1c
mwarko <marcantoinefournier@bettersec.fr>
2015-11-26 12:12:05 +0100

    sending mails with subject
    add a pwd a dest and a src

--- httpresp.py
+++ httpresp.py
@@ -1,17 +1,32 @@
 import urllib.request
 import urllib.parse
 import tkinter
-addr = "www.hakatecom.fr"
+import smtplib
+pref = 'http://www.'
+addr = 'google.fr/sweg'
+src = ''
+dest = ''
+pwd = ''
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
 
 def getresp(url):
 	try:
 		req = urllib.request.Request(url, headers = {'User-Agent' : 'Mozilla/5.0'})
 		print(urllib.request.urlopen(req).getcode())
-	except urllib.request.HTTPError as e:
-		print(e.getcode())
+	except urllib.error.HTTPError as e:
+		e = str(e.getcode())
+		msg = MIMEMultipart()
+		msg['From'] = 'MONITORING BETTERSEC'
+		msg['Subject'] = 'erreur de type ' + e + ' chez ' + addr
+		msg.attach(MIMEText('erreur de type ' + e + ' chez ' + addr))
+		server = smtplib.SMTP('smtp.gmail.com:587')
+		server.starttls()
+		server.login(src,pwd)
+		server.sendmail(src, dest, str(msg))
+		server.quit()
 
-
-getresp("http://hakatelecom.fr")
-
+getresp(pref + addr)
 
 
